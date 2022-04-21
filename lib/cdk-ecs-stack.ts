ced9af354247297175a26c8839bba1c4c2279418
liweiyi88 <weiyi.li713@gmail.com>
2022-04-21 20:20:26 +1000

    update

--- lib/cdk-ecs-stack.ts
+++ lib/cdk-ecs-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
-import { Cluster, ContainerImage, FargateTaskDefinition, LogDriver } from 'aws-cdk-lib/aws-ecs'
-import { InstanceType, Port, Vpc } from 'aws-cdk-lib/aws-ec2'
+import { Cluster, ContainerImage, LogDriver } from 'aws-cdk-lib/aws-ecs'
+import { Port, Vpc } from 'aws-cdk-lib/aws-ec2'
 import * as path from 'path'
 import { DockerImageAsset, NetworkMode } from 'aws-cdk-lib/aws-ecr-assets'
 import { NetworkLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
@@ -10,35 +10,29 @@ export class CdkEcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new Vpc(this, 'ECSVpc')
+    const vpc = new Vpc(this, 'ECSHelloRestApiVpc')
 
     const cluster = new Cluster(this, 'Cluster', {
       vpc,
     });
 
-    cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
-      instanceType: new InstanceType("t2.medium"),
-      desiredCapacity: 2,
-    });
-
-    const taskDefinition = new FargateTaskDefinition(this, 'TaskDef');
-
-    const asset = new DockerImageAsset(this, 'HelloRestApi', {
+    const asset = new DockerImageAsset(this, 'ECSHelloRestApiImage', {
       directory: path.join(__dirname, '/../docker'),
       networkMode: NetworkMode.HOST,
     })
 
-    taskDefinition.addContainer('RestApi', {
-      image: ContainerImage.fromDockerImageAsset(asset),
-      memoryLimitMiB: 256,
-      portMappings: [{containerPort: 80}],
-      logging: LogDriver.awsLogs({streamPrefix: 'HelloRestApi'})
-    });
-
     const service = new NetworkLoadBalancedFargateService(this, 'Service', {
       publicLoadBalancer: true,
+      memoryLimitMiB: 512,
+      desiredCount: 2,
       cluster,
-      taskDefinition
+      taskImageOptions: {
+        image: ContainerImage.fromDockerImageAsset(asset),
+        enableLogging: true,
+        containerName: 'HelloRestApi',
+        containerPort: 80,
+        logDriver: LogDriver.awsLogs({streamPrefix: 'ECSHelloRestApi'})
+      }
     });
 
     // The SG attached to service doesn't have any inbound rule, see https://github.com/aws/aws-cdk/issues/1490
